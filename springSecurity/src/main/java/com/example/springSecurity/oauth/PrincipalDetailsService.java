a6f6568dbbd639549da912a9a0684c48174ff0ee
alrdlrsha1 <alrdlrsha1@naver.com>
2022-08-08 14:27:42 +0900

    build file deploy options

---
+++ springSecurity/src/main/java/com/example/springSecurity/oauth/PrincipalDetailsService.java
@@ -0,0 +1,22 @@
+package com.example.springSecurity.oauth;
+
+import com.example.springSecurity.model.Member;
+import com.example.springSecurity.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member memberEntity = memberRepository.findByUsername(username);
+        return new PrincipalDetails(memberEntity);
+    }
+}
