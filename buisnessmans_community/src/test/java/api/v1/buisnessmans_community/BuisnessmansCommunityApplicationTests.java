0b5084e9603569a1dd9de87c872eb6ffd019ff01
alrdlrsha1 <alrdlrsha1@naver.com>
2022-08-17 11:26:31 +0900

    first commit

---
+++ buisnessmans_community/src/test/java/api/v1/buisnessmans_community/BuisnessmansCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package api.v1.buisnessmans_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuisnessmansCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
